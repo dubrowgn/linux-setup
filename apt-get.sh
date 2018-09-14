372684ab4a95a35b3d9f52037fdeb95b8c8be42f
Dustin Brown <dubrowgn@gmail.com>
2018-09-14 16:31:09 -0700

    Add node.js and rust to apt-get.sh

--- apt-get.sh
+++ apt-get.sh
@@ -2,9 +2,15 @@
 
 root_path="$(cd "$(dirname "$0")" && pwd)"
 
+# setup node.js repo
+curl -sL https://deb.nodesource.com/setup_10.x | \
+	sudo -E bash -
+
 # add sublime text repo
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | \
+	sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | \
+	sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # use Ubuntu firefox package
 sudo cp $root_path/etc/apt/preferences.d/* /etc/apt/preferences.d/.
@@ -32,6 +38,7 @@ sudo apt-get install \
 	firefox \
 	git \
 	htop \
+	nodejs \
 	speedcrunch \
 	sublime-text \
 	tree \
@@ -39,3 +46,6 @@ sudo apt-get install \
 
 sudo apt-get clean
 
+# install rust
+curl https://sh.rustup.rs -sSf | \
+	sh -s -- -y
