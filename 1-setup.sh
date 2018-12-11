162813a2908967766b5fcb217d0cf2eecab0812c
Dustin Brown <dubrowgn@gmail.com>
2018-12-11 10:55:58 -0800

    Split home into copy and append

--- 1-setup.sh
+++ 1-setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-root_path="$(cd "$(dirname "$0")" && pwd)"
+root_path="$(cd "$(dirname "$0")" && pwd)";
+home_path="$root_path/home";
 
-cat "$root_path/home/.bashrc" >> "$HOME/.bashrc"
+copy_path="$home_path/copy";
+for f in $(ls -A $copy_path); do
+	cp -i "$copy_path/$f" "$HOME/$f";
+done
+
+append_path="$home_path/append";
+for f in $(ls -A $append_path); do
+	cat "$append_path/$f" >> "$HOME/$f";
+done
