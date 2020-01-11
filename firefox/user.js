32d90a878690dd7e533f10c1379d4115e3a138b2
Dustin Brown <dubrowgn@gmail.com>
2020-01-11 12:26:21 -0700

    Add user.js for firefox

---
+++ firefox/user.js
@@ -0,0 +1,16 @@
+// 0 - allow auto-play (default)
+// 1 - do not auto-play
+// 2 - prompt user for auto-play
+user_pref("media.autoplay.default", 1);
+user_pref("media.autoplay.allow-muted", false);
+
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.urlbar.clickSelectsAll", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("findbar.highlightAll", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
