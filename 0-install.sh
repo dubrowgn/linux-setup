6ccab9ba5421c9b332275697d006f2edbacb479c
Dustin Brown <me@dubrowgn.com>
2020-08-23 11:27:48 -0600

    Update to node v14

--- 0-install.sh
+++ 0-install.sh
@@ -18,7 +18,7 @@ root_path="$(cd "$(dirname "$0")" && pwd)"
 opt_in_packages=()
 
 # setup node.js repo
-curl -sL https://deb.nodesource.com/setup_13.x | \
+curl -sL https://deb.nodesource.com/setup_14.x | \
 	sudo -E bash -
 
 # add sublime text repo
