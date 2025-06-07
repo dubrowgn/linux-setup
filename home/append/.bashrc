0d796ad353e0d103115c452630e2e66ba805c6fb
Dustin Brown <me@dubrowgn.com>
2025-06-07 09:07:12 -0600

    Reorganize bash includes

--- home/append/.bashrc
+++ home/append/.bashrc
@@ -1,10 +1,8 @@
 
 # custom
 
-export PATH="$HOME/.bin:$PATH"
-export PS1="\[\e[37m\e[1m\]\u\[\e[95m\]@\[\e[37m\]\h \[\e[95m\]\W\[\e[37m\] $\[\e[m\] "
-export TIMEFORMAT="real: %2Rs; user: %2Us; system: %2Ss; cpu: %P%%"
-
+shopt -s nullglob
 for f in "$HOME/.bash/"*; do
 	source "$f"
 done
+shopt -u nullglob
--- home/copy/.bash/init.sh
+++ home/copy/.bash/init.sh
@@ -7,3 +7,7 @@ alias fd="fdfind -uu -E .git"
 alias make="make -j$((2 * $(nproc)))"
 alias rg="rg -uu -g \!.git"
 alias zstd="zstd -T$(nproc)"
+
+export PATH="$HOME/.bin:$PATH"
+export PS1="\[\e[37m\e[1m\]\u\[\e[95m\]@\[\e[37m\]\h \[\e[95m\]\W\[\e[37m\] $\[\e[m\] "
+export TIMEFORMAT="real: %2Rs; user: %2Us; system: %2Ss; cpu: %P%%"
